1bde7f2e85838e7892fd4a8c0d52d29f4ec909db
carlpielou03 <tul35916@temple.edu>
2026-02-05 11:41:10 -0500

    Added the Main Get View without the Recycle Pattern.

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -3,23 +3,31 @@ package edu.temple.inclassuiacvitivity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.content.Context
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(_context: Context, _textSizes: Array<Int> ) : BaseAdapter() {
 
+    private val context = _context
+    private val textSizes = _textSizes
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return  textSizes.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return  textSizes[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+       return  position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = textSizes[position].toString()
+        textView.textSize = textSizes[position].toFloat()
+        textView.setPadding(5, 10, 0, 10)
+        return textView
     }
 
 }
